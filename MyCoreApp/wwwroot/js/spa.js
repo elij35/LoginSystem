9e4f108b83154b0705fd43cea544e4e740149056
Eli Bowen <elijbowen@gmail.com>
2022-11-10 20:34:21 +0000

    Made the website better with improved features

--- MyCoreApp/wwwroot/js/spa.js
+++ MyCoreApp/wwwroot/js/spa.js
@@ -4,9 +4,19 @@
 // Write your Javascript code.
 
 $(document).ready(function () {
+    $('#index').click(function () {
+        $('#page-content-wrapper').load('index.html');
+    })
+});
+
+$(document).ready(function () {
     $('#map').click(function () {
         $('#page-content-wrapper').load('map.html');
     })
 });
 
-
+$(document).ready(function () {
+    $('#game').click(function () {
+        $('#page-content-wrapper').load('game.html');
+    })
+});
