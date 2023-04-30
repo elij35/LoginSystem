a0412123c45ca3681269b5dab4872d4ea8b25486
Mohamad Trissi <mohamad.trissi@students.plymouth.ac.uk>
2023-04-30 17:04:58 +0100

    simple chat

---
+++ MyCoreApp/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using simplechat.Models;
+using simplechat.Services;
+
+namespace simplechat.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+       [HttpGet]
+       public IEnumerable<Users> Get()
+        {
+            return UserServices.GetData().ToArray();
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Users user)
+        {
+            UserServices.PushData(user);
+        }
+    }
+}
---
+++ MyCoreApp/Services/UsersServices.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using simplechat.Models;
+using Newtonsoft.Json;
+
+namespace simplechat.Services
+{
+    public class UserServices
+    {
+        private static List<Users> data = new List<Users>();
+
+        public static void Init()
+        {
+            string json = File.ReadAllText("wwwroot/data/data.json");
+            data = JsonConvert.DeserializeObject<List<Users>>(json);
+        }
+
+        public static List<Users> GetData()
+        {
+            return data;
+        }
+
+        public static void PushData(Users users)
+        {
+            data.Add(users);
+            File.WriteAllText("wwwroot/data/data.json", JsonConvert.SerializeObject(data));
+        }
+    }
+}
