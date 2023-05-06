9c83cda656643e5e0af7a5bcb2efafe021d233fc
Mohamad Trissi <mohamad.trissi@students.plymouth.ac.uk>
2023-05-06 19:33:01 +0100

    change the storage to local storage instead of storing the messages in <li> (now there are a record of the messages)and fixing some issues

--- MyCoreApp/wwwroot/js/masseging.js
+++ MyCoreApp/wwwroot/js/masseging.js
@@ -1,10 +1,58 @@
 ﻿// Write your Javascript code.
-var user = "";
+var Muser = '';
+var Suser = '';
+var user = '';
+var keybase = 'Cluster-F-Massege';
+
+function btnusers_onclick() {
+    Muser = document.getElementById('Muser').value;
+    Suser = document.getElementById('Suser').value;
+    if (Muser && Suser) {
+        if (Muser != Suser) {
+            document.getElementById('Muser').disabled = true;
+            document.getElementById('Suser').disabled = true;
+            document.getElementById('btnusers').disabled = true;
+
+            let names = keybase.concat(Muser + '-to-' + Suser);
+            let massege = [];
+            if (localStorage.getItem(names)) {
+                massege = JSON.parse(localStorage.getItem(names));
+                var Lenght = massege.length;
+                for (var i = 0; i < Lenght; i++) {
+                    var newm = document.createElement("li");
+                    newm.innerHTML = massege[i];
+                    m.appendChild(newm);
+                }
+            }
+
+            if (Muser == "Mike" || Suser == "Mike") {
+                user1.disabled = false;
+            }
+            if (Muser == "Mohamad" || Suser == "Mohamad") {
+                user3.disabled = false;
+            }
+            if (Muser == "Sam" || Suser == "Sam") {
+                user2.disabled = false;
+            }
+        } else {
+            alert('please type two different names!!');
+        }
+    } else {
+        alert('please type the name of the sender and reciver to get the chat');
+    }
+}
+
 function user1_onCilck() {
     user = "Mike";
-    user1.disabled = true;
-    user2.disabled = false;
-    user3.disabled = false;
+    if (Muser == "Mike" || Suser == "Mike") {
+        user1.disabled = true;
+    }
+    if (Muser == "Mohamad" || Suser == "Mohamad") {
+        user3.disabled = false;
+    }
+    if (Muser == "Sam" || Suser == "Sam") {
+        user2.disabled = false;
+    }
     t.disabled = false;
     t.value = "";
     b.disabled = false
@@ -12,9 +60,15 @@ function user1_onCilck() {
 }
 function user2_onCilck() {
     user = "Sam";
-    user3.disabled = false;
-    user2.disabled = true;
-    user1.disabled = false;
+    if (Muser == "Mike" || Suser == "Mike") {
+        user1.disabled = false;
+    }
+    if (Muser == "Mohamad" || Suser == "Mohamad") {
+        user3.disabled = false;
+    }
+    if (Muser == "Sam" || Suser == "Sam") {
+        user2.disabled = true;
+    }
     t.disabled = false;
     t.value = "";
     b.disabled = false;
@@ -22,9 +76,15 @@ function user2_onCilck() {
 }
 function user3_onCilck() {
     user = "Mohamad";
-    user3.disabled = true;
-    user2.disabled = false;
-    user1.disabled = false;
+    if (Muser == "Mike" || Suser == "Mike") {
+        user1.disabled = false;
+    }
+    if (Muser == "Mohamad" || Suser == "Mohamad") {
+        user3.disabled = true;
+    }
+    if (Muser == "Sam" || Suser == "Sam") {
+        user2.disabled = false;
+    }
     t.disabled = false;
     t.value = "";
     b.disabled = false;
@@ -32,10 +92,20 @@ function user3_onCilck() {
 }
 
 function b_onClick() {
+    let names = keybase.concat(Muser + '-to-' + Suser);
+    let massege = [];
     if (t.value) {
+        if (localStorage.getItem(names)) {
+            massege = JSON.parse(localStorage.getItem(names));
+        }
+        massege.push(user + ": " + t.value);
+        localStorage.setItem(names, JSON.stringify(massege));
+        massege = JSON.parse(localStorage.getItem(names));
+        var Lenght = massege.length;
         var newm = document.createElement("li");
-        newm.innerHTML = user + ": " + t.value;
+        newm.innerHTML = massege[Lenght - 1];
         m.appendChild(newm);
-        t.value = "";
+        document.getElementById('t').value = '';
     }
 }
+
