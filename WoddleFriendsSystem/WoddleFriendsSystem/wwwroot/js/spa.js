471f3885a8b8a0c28b5c5ad4f2ffcb8eceb8e3d8
BungeeDragon <skillmanesther@gmail.com>
2023-05-11 23:42:43 +0100

    SPA_01,SPA_02,SPA_03

---
+++ WoddleFriendsSystem/WoddleFriendsSystem/wwwroot/js/spa.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    $('#index').click(function () {
+        $('#page-content-wrapper').load('indexPanel.html');
+    })
+});
+$(document).ready(function () {
+    $('#users').click(function () {
+        $('#page-content-wrapper').load('users.html');
+    })
+});
+$(document).ready(function () {
+    $('#friends').click(function () {
+        $('#page-content-wrapper').load('friends.html');
+    })
+});
+$(document).ready(function () {
+    $('#posts').click(function () {
+        $('#page-content-wrapper').load('posts.html');
+    })
+});
+$(document).ready(function () {
+    $('#messaging').click(function () {
+        $('#page-content-wrapper').load('messaging.html');
+    })
+});
+$(document).ready(function () {
+    $('#games').click(function () {
+        $('#page-content-wrapper').load('games.html');
+    })
+});
