860421433ae574e7e279513b3e8254931441a006
Mohamad Trissi <mohamad.trissi@students.plymouth.ac.uk>
2023-05-12 20:32:09 +0100

    adding a security feature in order to encrypt the messages and decrypt it

--- MyCoreApp/wwwroot/js/masseging.js
+++ MyCoreApp/wwwroot/js/masseging.js
@@ -3,6 +3,11 @@ var Muser = '';
 var Suser = '';
 var user = '';
 var keybase = 'Cluster-F-Massege';
+const alphabet = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+    "u", "v", "w", "x", "y", "z",
+];
 
 function btnusers_onclick() {
     Muser = document.getElementById('Muser').value;
@@ -15,14 +20,18 @@ function btnusers_onclick() {
             document.getElementById('del').disabled = false;
             document.getElementById('rest').disabled = false;
 
+
             let names = keybase.concat(Muser + '-to-' + Suser);
             let massege = [];
+            var PMS;
             if (localStorage.getItem(names)) {
                 massege = JSON.parse(localStorage.getItem(names));
                 var Lenght = massege.length;
                 for (var i = 0; i < Lenght; i++) {
                     var newm = document.createElement("li");
-                    newm.innerHTML = massege[i];
+                    PMS = encrpt(massege[i], -2);
+
+                    newm.innerHTML = PMS;
                     m.appendChild(newm);
                 }
             }
@@ -96,17 +105,23 @@ function user3_onCilck() {
 
 function b_onClick() {
     let names = keybase.concat(Muser + '-to-' + Suser);
+    let names1 = keybase.concat(Suser + '-to-' + Muser);
     let massege = [];
     if (t.value) {
         if (localStorage.getItem(names)) {
             massege = JSON.parse(localStorage.getItem(names));
         }
-        massege.push(user + ": " + t.value);
+        var MS = user + ": " + t.value;
+        var SMS = encrpt(MS, 2);
+        var PMS;
+        massege.push(SMS);
         localStorage.setItem(names, JSON.stringify(massege));
+        localStorage.setItem(names1, JSON.stringify(massege));
         massege = JSON.parse(localStorage.getItem(names));
         var Lenght = massege.length;
         var newm = document.createElement("li");
-        newm.innerHTML = massege[Lenght - 1];
+        PMS = encrpt(massege[Lenght - 1], -2);
+        newm.innerHTML = PMS;
         m.appendChild(newm);
         document.getElementById('t').value = '';
     }
@@ -122,6 +137,7 @@ function rest_onClick() {
     while (m.firstChild) {
         m.removeChild(m.firstChild);
     }
+    N.innerHTML = "Welcome ";
 }
 
 function del_onClick() {
@@ -135,4 +151,36 @@ function del_onClick() {
     while (m.firstChild) {
         m.removeChild(m.firstChild);
     }
+    N.innerHTML = "Welcome ";
+}
+
+function encrpt(encrpt, key) {
+    const newword = [];
+    encrpt = encrpt.toLowerCase();
+    const char = encrpt.split('');
+    var L = char.length;
+    var LA = alphabet.length;
+    var Echar;
+    var Alph;
+    for (var i = 0; i < L; i++) {
+        Echar = char[i];
+        for (var c = 0; c < LA; c++) {
+            Alph = alphabet[c];
+            if (Echar == Alph) {
+                if (c + key >= LA) {
+                    newword[i] = alphabet[c + key - LA];
+                } else if (c + key < 0) {
+                    newword[i] = alphabet[LA + c + key];
+                } else {
+                    newword[i] = alphabet[c + key];
+                }
+                break;
+            } else {
+                newword[i] = char[i];
+            }
+        }
+    }
+    newword[0] = newword[0].toUpperCase();
+    var words = newword.join('');
+    return words;
 }
